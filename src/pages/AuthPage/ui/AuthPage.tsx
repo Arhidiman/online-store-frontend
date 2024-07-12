0309c0d7849be158a88a22ce7385ecad7e2eef4d
Dmitry <arhidiman@mail.ru>
2024-07-12 22:17:13 +0700

    fix: main page and auth page UI

--- src/pages/AuthPage/ui/AuthPage.tsx
+++ src/pages/AuthPage/ui/AuthPage.tsx
@@ -1,19 +1,20 @@
 import {RegistrationCard} from "@/pages/AuthPage/ui/RegistrationCard/RegistrationCard.tsx";
 import {AuthCard} from "@/pages/AuthPage/ui/AuthCard/AuthCard.tsx";
 import {useAuthPageStore} from "@/pages/AuthPage/store/useAuthPageStore.ts";
+import './AuthPage.scss'
 
 export const AuthPage = () =>  {
 
     const {isAuth} = useAuthPageStore()
 
     return (
-        <>
+        <div className='auth-container'>
             {
                 isAuth
                 ?<RegistrationCard/>
                 :<AuthCard/>
             }
-        </>
+        </div>
     )
 }
 
--- src/pages/Main/ui/Main.tsx
+++ src/pages/Main/ui/Main.tsx
@@ -1,10 +1,11 @@
 import {Products} from "@/modules/Products";
+import './Main.scss'
 
 export const Main = () =>  {
     return (
-        <>
+        <div className='main-container'>
             <Products/>
-        </>
+        </div>
     )
 }
 
