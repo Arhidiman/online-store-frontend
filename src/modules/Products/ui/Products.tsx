482ce73bd559dd2a9a79723cf83f391a85220b91
Dmitry <arhidiman@mail.ru>
2024-07-16 23:30:09 +0700

    feat: main page UI

--- src/modules/Products/ui/Products.tsx
+++ src/modules/Products/ui/Products.tsx
@@ -4,6 +4,7 @@ import {getLocalstorageItem} from "@/common/lib/getLocalstorageItem.ts";
 import {ProductCard} from "@/components/ProductCard/ProductCard.tsx"
 import mockProduct from '../mock/mockProduct.jpg'
 import "./Products.scss"
+import { Button } from "antd";
 
 export function Products() {
 
@@ -18,24 +19,39 @@ export function Products() {
     // console.log('toDoItems', toDoItems)
     console.log('user _id', userId)
 
-    const mockCards = [1,2,3,4,5]
+    const mockCards = [1,2,3,4,]
+
+    const cardContent = () => <div>
+        <img alt='product image' src={mockProduct}/>
+        <p>Цена: 1000000р</p>       
+        <p>Котяра необыкновенный</p>
+        <Button>Убрать в корзину</Button>
+    </div>
 
     return (
+
+
         <>
             <div>
-                <h2 className='goods-title'>Products</h2>
-                <div className='goods-container'>
-                    {
-                        mockCards.map(() =>
-                            <ProductCard
-                                name='Продам кота'
-                                description='description'
-                                cardSign={<span>+</span>}
-                                content={<img alt='product image' src={mockProduct}/>}
-                            />
-                        )
-                    }
+                <h2 className='products-title'>Products</h2>
+                <div className="products-content">
+                    <div className='products-cards'>
+                        {
+                            mockCards.map(() =>
+                                <ProductCard
+                                    name='уШЛЁПoк'
+                                    description='description'
+                                    cardSign={<span>+</span>}
+                                    content={cardContent()}
+                                />
+                            )
+                        }
+                    </div>
+                    <Button className="products_show-more" type="primary">Показать ещё</Button>
+
                 </div>
+             
+
             </div>
             <Modal
                 confirmHandler={closeModal}
